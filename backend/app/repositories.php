5dea0f5c39809cd1a9fb8f42e28f54202469fe48
Eliecer García Rey <egarciarey1981@gmail.com>
2024-10-05 19:50:08 +0200

    clean code

--- backend/app/repositories.php
+++ backend/app/repositories.php
@@ -2,21 +2,12 @@
 
 declare(strict_types=1);
 
-use Burger\Catalog\Domain\Model\Category\CategoryRepository;
-use Burger\Catalog\Domain\Model\Currency\CurrencyRepository;
-use Burger\Catalog\Domain\Model\Image\ImageRepository;
 use Burger\Catalog\Domain\Model\Product\ProductRepository;
-use Burger\Catalog\Infrastructure\Persistence\InMemory\InMemoryCategoryRepository;
-use Burger\Catalog\Infrastructure\Persistence\InMemory\InMemoryCurrencyRepository;
-use Burger\Catalog\Infrastructure\Persistence\InMemory\InMemoryImageRepository;
 use Burger\Catalog\Infrastructure\Persistence\InMemory\InMemoryProductRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        CategoryRepository::class => \DI\autowire(InMemoryCategoryRepository::class),
-        CurrencyRepository::class => \DI\autowire(InMemoryCurrencyRepository::class),
-        ImageRepository::class => \DI\autowire(InMemoryImageRepository::class),
         ProductRepository::class => \DI\autowire(InMemoryProductRepository::class),
     ]);
 };
