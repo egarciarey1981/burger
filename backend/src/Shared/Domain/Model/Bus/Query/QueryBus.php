4f9cdd84d84907433ea3eebb17802622d07642ec
Eliecer García Rey <erey@freepik.com>
2024-10-09 11:21:21 +0200

    interface QueryBus

--- backend/src/Shared/Domain/Model/Bus/Query/QueryBus.php
+++ backend/src/Shared/Domain/Model/Bus/Query/QueryBus.php
@@ -4,5 +4,6 @@ namespace Burger\Shared\Domain\Model\Bus\Query;
 
 interface QueryBus
 {
+    public function register(string $queryClass, QueryHandler $handlerClass);
     public function handle(Query $query);
 }
